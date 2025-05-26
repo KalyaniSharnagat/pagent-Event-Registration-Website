b4d0a11a37876f917fb71f396a5972984f081490
Vishal Mirase <vishalrsinfo09@gmail.com>
2025-05-26 10:58:39 +0530

    navbar changes

--- Javascript/script.js
+++ Javascript/script.js
@@ -13,3 +13,15 @@ AOS.init({
   once: true,
   duration: 1200
 });
+
+
+const navbarNav = document.getElementById('navbarNav');
+const navbarToggler = document.querySelector('.navbar-toggler');
+
+navbarNav.addEventListener('show.bs.collapse', () => {
+  navbarNav.style.backgroundColor = '#f8f9fa';
+});
+
+navbarNav.addEventListener('hide.bs.collapse', () => {
+  navbarNav.style.backgroundColor = 'transparent';
+});
